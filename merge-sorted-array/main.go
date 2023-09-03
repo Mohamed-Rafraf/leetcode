46c073b0a750d9646d40533b9bff098077745208
Mohamed Rafraf <mohamedrafraf@insat.u-carthage.tn>
2023-09-03 17:56:44 +0100

    Merge Sorted Array

---
+++ merge-sorted-array/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	array1 := []int{1, 2, 3, 0, 0, 0}
+	array2 := []int{2, 5, 6}
+	fmt.Println("1st array: ", array1)
+	fmt.Println("2nd array: ", array2)
+	merge(array1, 3, array2, 3)
+	fmt.Println("result   : ", array1)
+
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for n > 0 {
+		if nums1[m-1] > nums2[n-1] {
+			nums1[m+n-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[m+n-1] = nums2[n-1]
+			n--
+		}
+	}
+}
